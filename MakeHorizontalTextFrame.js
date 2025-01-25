17c1f465f7ea6d0acd0872f95aca7abe2ae313d3
meerkatotter <meerkatmanga@gmail.com>
2025-01-25 20:39:37 +0700

    feat: add script to make horizontal text frames

---
+++ MakeHorizontalTextFrame.js
@@ -0,0 +1,135 @@
+var doc = app.activeDocument;
+var bookSize = doc.pages.count();
+var prefs = doc.documentPreferences;
+
+// Adjustable parameters
+var heightRatio = 1;
+var widthRatio = 2;
+
+function getAllParagraphStyles(styleGroup) {
+    var allStyles = [];
+    var allStylesNames = [];
+
+    for (var i = 0; i < styleGroup.paragraphStyles.length; i++) {
+        allStyles.push(styleGroup.paragraphStyles[i]);
+        allStylesNames.push(styleGroup.paragraphStyles[i].name);
+    }
+    
+    for (var j = 0; j < styleGroup.paragraphStyleGroups.length; j++) {
+        groupParagraphStyles = getAllParagraphStyles(styleGroup.paragraphStyleGroups[j]);
+        allStyles = allStyles.concat(groupParagraphStyles[0]);
+        allStylesNames = allStylesNames.concat(groupParagraphStyles[1]);
+    }
+    
+    return [allStyles, allStylesNames];
+}
+
+function getAllObjectStyles(styleGroup) {
+    var allStyles = [];
+    var allStylesNames = [];
+
+    for (var i = 0; i < styleGroup.objectStyles.length; i++) {
+        allStyles.push(styleGroup.objectStyles[i]);
+        allStylesNames.push(styleGroup.objectStyles[i].name);
+    }
+    
+    for (var j = 0; j < styleGroup.objectStyleGroups.length; j++) {
+        groupObjectStyles = getAllParagraphStyles(styleGroup.objectStyleGroups[j]);
+        allStyles = allStyles.concat(groupObjectStyles[0]);
+        allStylesNames = allStylesNames.concat(groupObjectStyles[1]);
+    }
+    
+    return [allStyles, allStylesNames];
+}
+
+function applyParagraphStyleToTextFrame(textFrame, paragraphStyle) {
+    try {
+        if (!paragraphStyle.isValid) {
+            alert("Paragraph style does not exist.");
+            return;
+        }
+        textFrame.contents = "A";
+        textFrame.paragraphs.everyItem().appliedParagraphStyle = paragraphStyle;
+        textFrame.contents = "";
+    } catch (e) {
+        alert("An error occurred: " + e.message);
+    }
+}
+
+function makeHorizontalTextFrame(targetStyle, targetLayer) {
+    try {
+        for (var i = 0; i < bookSize; i++) {
+            var page = doc.pages[i];
+            app.activeWindow.activePage = page;
+
+            // avoid pass by reference
+            var pageItemsOriginal = page.pageItems;
+            var pageItems = [];
+            for (var j = 0; j < pageItemsOriginal.length; j++) {
+                pageItems.push(pageItemsOriginal[j]);
+            }
+    
+            for (var j = 0; j < pageItems.length; j++) {
+                var pageItem = pageItems[j];
+                var pageElement = pageItem.getElements()[0];
+                if (!pageItem.itemLayer.locked && !pageItem.locked && pageElement instanceof TextFrame) {
+                    var bounds = pageItem.geometricBounds;
+                    var height = bounds[2] - bounds[0];
+                    var width = bounds[3] - bounds[1];
+
+                    var heightDiff = ((height * heightRatio) - height) / 2;
+                    var widthDiff = ((width * widthRatio) - width) / 2;
+
+                    bounds[0] = bounds[0] - heightDiff;
+                    bounds[2] = bounds[2] + heightDiff;
+                    bounds[1] = bounds[1] - widthDiff;
+                    bounds[3] = bounds[3] + widthDiff;
+
+                    if (targetLayer.isValid) {
+                        var textFrame = page.textFrames.add();
+                        textFrame.geometricBounds = bounds;
+                        textFrame.itemLayer = targetLayer;
+                        applyParagraphStyleToTextFrame(textFrame, targetStyle);
+                    }
+                }
+            }
+        }
+    } catch (err) {
+        alert(err)
+    }
+}
+
+function main() {
+    var targetStyle = null;
+    var targetLayer = null;
+
+    w = new Window ("dialog", "Choose Paragraph Style and Layer", undefined, {closeButton: true});
+    w.orientation = "row";
+    w.alignChildren = ["right", "center"];
+
+    w.add("statictext", undefined, "Make text frames with paragraph style");
+    var paragraphStyles = getAllParagraphStyles(doc);
+    var paragraphStylesNames = paragraphStyles[1];
+    var paragraphStyles = paragraphStyles[0];
+    paragraphStyle = w.add("dropdownlist", undefined, paragraphStylesNames);
+    paragraphStyle.selection = 0;
+
+    w.add("statictext", undefined, "to layer");
+    moveToLayer = w.add("dropdownlist", undefined, app.activeDocument.layers.everyItem().name);
+    moveToLayer.selection = 0;
+
+    button = w.add ("button", [0,0,96,20], "OK", {name: "Ok"});
+
+    button.onClick = function() {
+        targetStyle = paragraphStyles[paragraphStyle.selection.index];
+        targetLayer = doc.layers.item(moveToLayer.selection.text);
+        exit();
+    }
+    w.show();
+
+    if (targetStyle && targetLayer) {
+        makeHorizontalTextFrame(targetStyle, targetLayer);
+    }
+}
+
+main();
--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # Manga InDesign Scripts
 
+#### MakeHorizontalTextFrame
+To make horizontal text frames in the same position as existing vertical text frames in an InDesign document
+1. Make sure all vertical text frames have the default text frame object style applied
+2. Edit the text frame object style to open Object Style Options, and set Auto-Sizing in Text Frame Auto Size Options to "Height and Width" with "No Line Breaks" enabled
+    - Choose the auto-sizing source direction according to how the vertical text frames are centered in the document
+    - For horizontally center-aligned frames, choose the arrow in the top center
+    - For horizontally top-aligned frames, choose the arrow in the top right
+3. Edit the text frame object style again to turn off Auto-Sizing in Text Frame Auto Size Options so that the created horizontal text frames are not affected
+4. Run the script and choose the paragraph style to be applied to the created text frames as well as the target layer
+
+Note: You can also adjust the height as well as the width ratio of the created horizontal text frames to the original vertical text frames through the ["Adjustable parameters" section](MakeHorizontalTextFrame.js#L5) of the script
+
 #### ReversePageNumberer
+To add page numbers to an InDesign document with a backward left-to-right layout
 1. Create a new parent page, for example: Prefix "C" with the name "PageNumber"
 2. Create a new layer for page numbers, for example: "Page Numbers"
 3. Inside the new parent, insert blank text frames on the left and right pages with your page number's preferred style or formatting
 4. For each page you'd like to insert a page number on, put the page in focus and run the script
     - You can also select individual pages through the pages panel by double-clicking a page then then running the script
 
-Note: You can also adjust your page number layer and parent names as well as the starting page number through the ["Adjustable parameters" section](ReversePageNumberer.js#L6) of the script
+Note: You can also adjust the page number layer, parent names, starting page number, and the padding option through the ["Adjustable parameters" section](ReversePageNumberer.js#L6) of the script
